96a8df6d659f8487812bda50f7e13f7088a27da7
vscicchi <128091367+vscicchi@users.noreply.github.com>
2023-09-25 20:57:15 +0000

    Updated Interpreter

--- lispy/lis.py
+++ lispy/lis.py
@@ -6,7 +6,23 @@ Number = (int, float)     # A Scheme Number is implemented as a Python int or fl
 Atom   = (Symbol, Number) # A Scheme Atom is a Symbol or Number
 List   = list             # A Scheme List is implemented as a Python list
 Exp    = (Atom, List)     # A Scheme expression is an Atom or List
-Env    = dict             # A Scheme environment (defined below)
+# Env    = dict             # A Scheme environment (defined below)
+
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs, with an outer Env."
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        return self if (var in self) else self.outer.find(var)
+
+class Procedure(object):
+    "A user-defined Scheme procedure."
+    def __init__(self, parms, body, env):
+        self.parms, self.body, self.env = parms, body, env
+    def __call__(self, *args): 
+        return eval(self.body, Env(self.parms, args, self.env))
 
 def standard_env() -> Env:
     "An environment with some Scheme standard procedures."
@@ -41,6 +57,7 @@ def standard_env() -> Env:
     })
     return env
 
+# Define an environment for the program
 global_env = standard_env()
 
 def eval(x: Exp, env=global_env) -> Exp:
@@ -48,7 +65,9 @@ def eval(x: Exp, env=global_env) -> Exp:
     if isinstance(x, Symbol):        # variable reference
         return env[x]
     elif isinstance(x, Number):      # constant number
-        return x                
+        return x
+    elif x[0] == 'quote':
+        return x[1]             
     elif x[0] == 'if':               # conditional
         (_, test, conseq, alt) = x
         exp = (conseq if eval(test, env) else alt)
@@ -95,6 +114,20 @@ def atom(token: str) -> Atom:
         except ValueError:
             return Symbol(token)
 
+def repl(prompt='lis.py> '):
+    "A prompt-read-eval-print loop."
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None: 
+            print(schemestr(val))
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
+
 def test_tokenize():
     program = "(begin (define r 10) (* pi (* r r)))"
     assert tokenize(program) == ['(', 'begin', '(', 'define', 'r', '10', ')', '(',
@@ -115,9 +148,16 @@ def test_eval():
     assert eval(eval(parse("(begin (define r 10) (* pi (* r r)))"))) == 314.1592653589793
     print("Eval Works")
 
+def test_schemestr():
+    expr = ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
+    assert schemestr(expr) == "(begin (define r 10) (* pi (* r r)))"
+    print("Schemestr works")
+
 if __name__ == "__main__":
     test_tokenize()
     test_parse()
     test_global_env()
     test_eval()
+    test_schemestr()
     print("Complete")
+    repl()
